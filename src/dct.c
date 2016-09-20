e7a3df8934bde5594e19a7b4bc0de2b3364a78ae
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-09-20 14:15:03 -0300

    todo

---
+++ src/dct.c
@@ -0,0 +1,17 @@
+/**
+ * @brief Applies a DCT to an array.
+ *
+ * @param a Target array.
+ * @param n Array size.
+ */
+void dct(int *a, int n)
+{
+	// TODO.
+}
+
+/**
+ * @brief Unit testing for dct().
+ */
+void test_dct(void)
+{
+}
---
+++ src/quantization.c
@@ -0,0 +1,18 @@
+/**
+ * @brief Applies quantization to an array.
+ *
+ * @brief a Target array.
+ * @param n Array size.
+ * @param q Quantization to apply.
+ */
+void quantization(int *a, int n, double q)
+{
+	/* TODO */
+}
+
+/**
+ * @brief Unit testing for quantization.
+ */
+void test_quantization(void)
+{
+}
